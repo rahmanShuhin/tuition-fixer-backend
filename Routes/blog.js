076ebd84ae124d60c44846fbe9cdedd91bd491c3
rahmanShuhin <suhin4000@gmail.com>
2021-03-12 10:59:02 +0600

    blog added route

--- Routes/blog.js
+++ Routes/blog.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const MongoClient = require("mongodb").MongoClient;
+const { ObjectID } = require("mongodb");
 require("dotenv").config();
 
 const uri = process.env.URI;
+
 router.get("/", (req, res) => {
   let client = new MongoClient(uri, {
     useNewUrlParser: true,
@@ -33,7 +35,9 @@ router.post("/post", (req, res) => {
       console.log(err);
     } else {
       const data = req.body;
-      data.date = new date();
+      data.date = new Date().toDateString();
+      data.approved = false;
+      console.log(req.body);
       const collection = client.db("TUITION").collection("BLOGS");
       collection.insertOne(data, (err, documents) => {
         if (err) {
@@ -45,6 +49,36 @@ router.post("/post", (req, res) => {
           client.close();
         }
       });
+    }
+  });
+});
+
+router.patch("/update/:id", (req, res) => {
+  console.log(req.body);
+  console.log(req.params.id);
+  let client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  client.connect((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const collection = client.db("TUITION").collection("BLOGS");
+      collection
+        .updateOne(
+          { _id: ObjectID(req.params.id) },
+          {
+            $set: {
+              approved: true,
+            },
+            $currentDate: { lastModified: true },
+          }
+        )
+        .then(function (result) {
+          res.send("success");
+          client.close();
+        });
     }
   });
 });
